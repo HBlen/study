54a585e416c29222b1024181b3d6a6157a2a2adf
hanzhengbin <1197379785@qq.com>
2019-07-30 15:21:55 +0800

    Spring 依赖注入

---
+++ 20190730/spring-di/src/com/blen/innerbeans/MainApp.java
@@ -0,0 +1,19 @@
+package com.blen.innerbeans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+  public static void main(String[] args){
+    //自己新建对象，控制对象生成
+//    TextEditor textEditor = new TextEditor();
+//    SpellChecker spellChecker = new SpellChecker();
+//    textEditor.setSpellChecker(spellChecker);
+//    textEditor.spellCheck();
+
+    //IOC 控制反转
+    ApplicationContext context = new ClassPathXmlApplicationContext("BeansInner.xml");
+    TextEditor te=(TextEditor) context.getBean("textEditor");
+    te.spellCheck();
+  }
+}
