d25c07bda93fd69b504d85fd8ab590226a5d3952
hanzhengbin <hanzhengbin@huobi.com>
2019-08-04 18:01:53 +0800

    spring

---
+++ 20190723/configbybean/src/com/blen/spring/autowired/SpellChecker.java
@@ -0,0 +1,10 @@
+package com.blen.spring.autowired;
+
+public class SpellChecker {
+  public SpellChecker(){
+    System.out.println("Inside SpellChecker constructor." );
+  }
+  public void checkSpelling(){
+    System.out.println("Inside checkSpelling." );
+  }
+}
