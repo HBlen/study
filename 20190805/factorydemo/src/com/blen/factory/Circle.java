cc443261ae2a8b4f9274126b14975d1d0bf8a361
hanzhengbin <1197379785@qq.com>
2019-08-05 23:48:57 +0800

    工程模式

---
+++ 20190805/factorydemo/src/com/blen/factory/Circle.java
@@ -0,0 +1,10 @@
+package com.blen.factory;
+
+import com.blen.factory.Shape;
+
+public class Circle implements Shape {
+  @Override
+  public void draw(){
+    System.out.println("Inside Circle::draw() method.");
+  }
+}
