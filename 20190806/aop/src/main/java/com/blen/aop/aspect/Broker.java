6873a86363134c54cd82f9efc3a6a9c8ae921f95
hanzhengbin <1197379785@qq.com>
2019-08-10 15:54:57 +0800

    @around AOP

--- 20190806/aop/src/main/java/com/blen/aop/aspect/Broker.java
+++ 20190806/aop/src/main/java/com/blen/aop/aspect/Broker.java
@@ -2,23 +2,45 @@ package com.blen.aop.aspect;
 
 import sun.jvm.hotspot.debugger.cdbg.Sym;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class Broker {
 
-  @Before("execution(* com.blen.aop.pojo.Landlord.service())")
-  public void before(){
+  /**
+   * @Before ,@After
+   */
+//  @Before("execution(* com.blen.aop.pojo.Landlord.service())")
+//  public void before(){
+//    System.out.println("带租客看房");
+//    System.out.println("谈价格");
+//  }
+//
+//  @After("execution(* com.blen.aop.pojo.Landlord.service())")
+//  public void after(){
+//    System.out.println("交钥匙");
+//  }
+
+  //@Around
+  @Around("execution(* com.blen.aop.pojo.Landlord.service())")
+  public void around(ProceedingJoinPoint joinPoint){
     System.out.println("带租客看房");
     System.out.println("谈价格");
-  }
 
-  @After("execution(* com.blen.aop.pojo.Landlord.service())")
-  public void after(){
+    try{
+      joinPoint.proceed();
+    }catch (Throwable throwable){
+      throwable.printStackTrace();
+    }
+
     System.out.println("交钥匙");
   }
+
 }
